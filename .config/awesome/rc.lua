08356a3e2d86e4ce4141c53779a2bed1e351389f
Sascha Holzleiter <sascha@root-login.org>
2018-04-16 01:46:34 +0200

    use urxvt and gvim, also change terminal keybinding

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -44,9 +44,9 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
+terminal = "urxvt"
+editor = os.getenv("EDITOR") or "gvim"
+editor_cmd = editor
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
@@ -276,7 +276,7 @@ globalkeys = gears.table.join(
         {description = "go back", group = "client"}),
 
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
+    awful.key({ modkey,           }, "s", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
