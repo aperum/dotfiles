0ea8242dcfea1a0e049e3f584648bde069811d5f
Sascha Holzleiter <sascha@root-login.org>
2018-04-16 01:39:38 +0200

    remove keyboard layout widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -113,9 +113,6 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
 -- {{{ Wibar
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
@@ -216,7 +213,6 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
