9a2774b513540eaed4989c6cc978f8135f8c2e4d
Sascha Holzleiter <sascha@root-login.org>
2018-04-16 02:12:29 +0200

    add altkey and change popup keybinding

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -53,6 +53,7 @@ editor_cmd = editor
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
+altkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
@@ -228,7 +229,7 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
-    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
+    awful.key({ modkey, altkey    }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
               {description = "view previous", group = "tag"}),
