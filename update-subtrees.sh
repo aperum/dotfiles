44a099fcc6bdac0368d5f38c42abceededa41de5
Sascha Holzleiter <sascha@root-login.org>
2022-04-06 14:14:15 +0200

    fix config dir rename mess

--- update-subtrees.sh
+++ update-subtrees.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-git subtree pull --prefix .configs/awesome/awesome-wm-widgets https://github.com/streetturtle/awesome-wm-widgets master --squash
-git subtree pull --prefix .configs/awesome/battery-widget https://github.com/deficient/battery-widget master --squash
-git subtree pull --prefix .configs/awesome/calendar https://github.com/deficient/calendar master --squash
-git subtree pull --prefix .configs/awesome/freedesktop https://github.com/lcpz/awesome-freedesktop master --squash
-git subtree pull --prefix .configs/awesome/json https://github.com/rxi/json.lua master --squash
-git subtree pull --prefix .configs/awesome/lain https://github.com/lcpz/lain master --squash
-git subtree pull --prefix .configs/awesome/vicious https://github.com/vicious-widgets/vicious master --squash
-git subtree pull --prefix .configs/awesome/layout-machi https://github.com/xinhaoyuan/layout-machi master --squash
+git subtree pull --prefix .config/awesome/awesome-wm-widgets https://github.com/streetturtle/awesome-wm-widgets master --squash
+git subtree pull --prefix .config/awesome/battery-widget https://github.com/deficient/battery-widget master --squash
+git subtree pull --prefix .config/awesome/calendar https://github.com/deficient/calendar master --squash
+git subtree pull --prefix .config/awesome/freedesktop https://github.com/lcpz/awesome-freedesktop master --squash
+git subtree pull --prefix .config/awesome/json https://github.com/rxi/json.lua master --squash
+git subtree pull --prefix .config/awesome/lain https://github.com/lcpz/lain master --squash
+git subtree pull --prefix .config/awesome/vicious https://github.com/vicious-widgets/vicious master --squash
+git subtree pull --prefix .config/awesome/layout-machi https://github.com/xinhaoyuan/layout-machi master --squash
 
 git subtree pull --prefix .tmux/plugins/tpm https://github.com/tmux-plugins/tpm master --squash
 
